9b40060da627786b1cf045113e3ac581822ba708
croissant <wathyic@gmail.com>
2022-10-18 11:36:15 +0800

    fix: fix bug

--- index.js
+++ index.js
@@ -78,11 +78,12 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const PORT = process.env.PORT | 9000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
