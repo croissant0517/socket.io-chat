2e95b91c7add0c4ffa554d08be1279d1203b26b5
croissant <wathyic@gmail.com>
2022-10-20 22:53:45 +0800

    fix: fix bug

--- server.js
+++ server.js
@@ -5,67 +5,11 @@ const app = express();
 const PORT = process.env.PORT | 9000;
 const { Server } = require("socket.io");
 const { instrument } = require("@socket.io/admin-ui");
-const server = app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
-
-const io = new Server(server, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-});
 
 app.get("/", (req, res) => {
   res.send("It is working!");
 });
 
-io.use((socket, next) => {
-  const room = socket.handshake.auth.room;
-  socket.room = room;
-  next();
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
-
-// when client connected
-io.on("connection", (socket) => {
-  console.log(`${socket.id} connected`);
-  if (socket.room) {
-    socket.join(socket.room);
-    socket.in(socket.room).emit("receive message", `${socket.id} join Room!`);
-  } else {
-    socket.join("Piblic Room");
-    socket.to("Piblic Room").emit("receive message", `${socket.id} join Room!`);
-  }
-
-  // when client disconnected
-  socket.on("disconnect", () => {
-    if (socket.room) {
-      socket
-        .to(socket.room)
-        .emit("receive message", `${socket.id} leave chat!`);
-    } else {
-      socket
-        .to("Piblic Room")
-        .emit("receive message", `${socket.id} leave chat!`);
-    }
-  });
-
-  // when client send a message
-  socket.on("chat message", (msg) => {
-    // log message on server
-    console.log("message: " + msg);
-
-    // send message to every client
-    // io.emit("receive message", msg);
-    if (socket.room) {
-      socket.to(socket.room).emit("receive message", msg);
-    } else {
-      socket.to("Piblic Room").emit("receive message", msg);
-    }
-
-    // send message to every client except the client who sended message
-    // socket.broadcast.emit("receive message", msg);
-  });
-});
-
-instrument(io, { auth: false });
